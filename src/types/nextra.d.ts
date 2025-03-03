a2eb851102ee31fae0871dfa34046fd6cae22f17
Julian Holguin <93228068+jholguinx@users.noreply.github.com>
2025-03-03 18:46:04 -0500

    fix222

--- src/types/nextra.d.ts
+++ src/types/nextra.d.ts
@@ -1,6 +1,9 @@
 declare module 'nextra' {
   import type { NextConfig } from 'next';
 
-  const nextra: (options?: Record<string, unknown>) => NextConfig;
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  const nextra: (options?: any) => NextConfig;
+  /* eslint-enable @typescript-eslint/no-explicit-any */
+
   export default nextra;
 }
